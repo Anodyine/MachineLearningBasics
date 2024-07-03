2fcca9a0e4bd26dbf9357b4013cc1c59a22c2d3d
Kyle Merritt <ky563720@ucf.edu>
2024-07-03 15:31:29 -0400

    solved the plotting xor scatter problem

--- Basics/Plotting/generate_and_plot_xor.py
+++ Basics/Plotting/generate_and_plot_xor.py
@@ -1,4 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+X = 2*np.random.random((1000,2)) - 1
+print(X)
+
+def getColor (x):
+    return 50 if (x[0] > 0 and x[1] < 0) or (x[0] < 0 and x[1] > 0) else 0
+getColors = np.vectorize(getColor, signature='(n)->()')
+
+Y = getColors(X)
+
+plt.scatter(X[:,0], X[:,1], c=Y)
+plt.show()
 
