7f0f0e282e48e6b9f65a4e4f8fef65e2f69b7546
Kyle Merritt <kyle.merritt@knights.ucf.edu>
2024-07-28 14:01:42 -0400

    renamed train to informativeFeatureIdentification

--- LinearRegression/informativeFeatureIdentification.py
+++ LinearRegression/informativeFeatureIdentification.py
@@ -11,17 +11,25 @@ X, y = datasets.make_regression(n_samples=200, n_features=5, n_informative=1, no
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 fig = plt.figure(figsize=(8,6))
 
-colors = ['maroon', 'sienna', 'steelblue', 'darkolivegreen','purple']
+dotColors = ['maroon', 'sienna', 'steelblue', 'darkolivegreen','purple']
 for i in range(X.shape[1]):
-    plt.scatter(X[:,i], y, color = colors[i], marker = "o", s = 30)
+    plt.scatter(X[:,i], y, color = dotColors[i], marker = "o", s = 30)
 
 model = LinearRegression(learningRate = 0.005, numIterations = 10000)
 
 model.fit(X_train, y_train)
 preditions = model.predictFeatures(X_test)
 colors = ['red', 'orange',  'lightskyblue', 'limegreen','violet']
+lowestErrorIndex = 0
+lowestError = None; 
 for i in range(X.shape[1]):
-    print("Error of " + colors[i] + ": " + str(meanSquaredError(y_test, preditions[:,i])))
+    error = meanSquaredError(y_test, preditions[:,i])
+    if lowestError == None or error < lowestError:
+        lowestError = error
+        lowestErrorIndex = i
+    print("Error of " + colors[i] + ": " + str(error))
     plt.plot(X_test[:,i], preditions[:,i], color=colors[i], linewidth = 2)
 
+print("Lowest error = " + str(lowestError))
+print(colors[lowestErrorIndex] + " is the informative feature.")
 plt.show()
